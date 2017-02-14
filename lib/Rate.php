c32f71e0b29f49b9d6e3ea4d10baa305c3bc75eb
AJ Quick <ajquick@gmail.com>
2017-02-13 22:27:18 -0700

    Update Rate
    Add copyright header. Add on fields for validation.

--- lib/Rate.php
+++ lib/Rate.php
@@ -9,14 +9,13 @@
  *
  * © 2017 Multidimension.al - All Rights Reserved
  * 
- * NOTICE:  All information contained herein is, and remains
- * the property of Multidimension.al and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to Multidimension.al and its suppliers
- * and may be covered by U.S. and Foreign Patents, patents in
- * process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained.
+ * NOTICE:  All information contained herein is, and remains the property of
+ * Multidimension.al and its suppliers, if any.  The intellectual and
+ * technical concepts contained herein are proprietary to Multidimension.al
+ * and its suppliers and may be covered by U.S. and Foreign Patents, patents in
+ * process, and are protected by trade secret or copyright law. Dissemination
+ * of this information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained.
  */
 
 namespace Multidimensional\Usps;
@@ -36,6 +35,16 @@ class Rate extends Usps
     protected $packages = [];
     
     protected $revision = 2;
+    
+    public $fields = [
+        'Revison' => [
+            'type' => 'integer'
+        ],
+        'Package' => [
+            'type' => 'Package',
+            'fields' => (new Package)->fields;
+        ]
+    ];
 
     public function __construct(array $config = [])
     {
