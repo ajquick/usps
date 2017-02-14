14f594cf01eeeda7222f9f84febfa33a8dcda15a
AJ Quick <ajquick@gmail.com>
2017-02-13 22:26:33 -0700

    Update AddressValidate
    Add copyright header. Add all methods for the XML code. Change way array
    is constructed. Add public validate function. Add result parser.

--- lib/AddressValidate.php
+++ lib/AddressValidate.php
@@ -1,22 +1,27 @@
 <?php
-/**
+/**    __  ___      ____  _     ___                           _                    __
+ *    /  |/  /_  __/ / /_(_)___/ (_)___ ___  ___  ____  _____(_)___  ____   ____ _/ /
+ *   / /|_/ / / / / / __/ / __  / / __ `__ \/ _ \/ __ \/ ___/ / __ \/ __ \ / __ `/ / 
+ *  / /  / / /_/ / / /_/ / /_/ / / / / / / /  __/ / / (__  ) / /_/ / / / // /_/ / /  
+ * /_/  /_/\__,_/_/\__/_/\__,_/_/_/ /_/ /_/\___/_/ /_/____/_/\____/_/ /_(_)__,_/_/   
+ *                                                                                  
  * CONFIDENTIAL
  *
  * © 2017 Multidimension.al - All Rights Reserved
  * 
- * NOTICE:  All information contained herein is, and remains
- * the property of Multidimension.al and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to Multidimension.al and its suppliers
- * and may be covered by U.S. and Foreign Patents, patents in
- * process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained.
+ * NOTICE:  All information contained herein is, and remains the property of
+ * Multidimension.al and its suppliers, if any.  The intellectual and
+ * technical concepts contained herein are proprietary to Multidimension.al
+ * and its suppliers and may be covered by U.S. and Foreign Patents, patents in
+ * process, and are protected by trade secret or copyright law. Dissemination
+ * of this information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained.
  */
 
 namespace Multidimensional\Usps;
 
 use Multidimensional\Usps\Address;
+use Multidimensional\XmlArray;
 
 class AddressValidate extends Usps
 {
@@ -27,6 +32,19 @@ class AddressValidate extends Usps
     
     private $includeOptionalElements = false;
     private $returnCarrierRoute = false;
+    
+    public $fields = [
+        'IncludeOptionalElements' => [
+            'type' => 'boolean'
+        ],
+        'ReturnCarrierRoute' => [
+            'type' => 'boolean'
+        ],
+        'Address' => [
+            'type' => 'Address',
+            'fields' => (new Address)->fields;
+        ]
+    ];
     
     public function __construct(array $config = [])
     {
@@ -47,21 +65,27 @@ class AddressValidate extends Usps
         }
     }
     
+    /**
+     * @param bool $boolean
+     */
     public function setIncludeOptionalElements($boolean)
     {
-        $this->includeOptionalElements = (boolean) $boolean;
+        $this->includeOptionalElements = (bool) $boolean;
     }
     
+    /**
+     * @param bool $boolean
+     */
     public function setReturnCarrierRoute($boolean)
     {
-        $this->returnCarrierRoute = (boolean) $boolean;
+        $this->returnCarrierRoute = (bool) $boolean;
     }
     
     /**
      * @return array
      */
-    private function buildArray()
-    {
+    public function toArray()
+    {        
         $array = [];
         if ($this->includeOptionalElements === true) {
             $array['IncludeOptionalElements'] = 'true';
@@ -79,21 +103,29 @@ class AddressValidate extends Usps
     /**
      * @return array
      */
-    public function toArray()
-    {
-        return buildArray();
-    }
-    
-    /**
-     * @return array
-     */
     public function validate()
     {
         $xml = $this->buildXML($this->toArray());
         if ($this->validateXML($xml, $this->apiClass)) {
-            return $this->request($this->apiClass);
+            $result = $this->request($this->apiClass);
+            return $this->parseResult($result);
         }else{
             return false;    
         }
+    }
+    
+    /**
+     * @param string $result
+     * @return array
+     */
+    private function parseResult($result)
+    {
+        $array = (new XMLArray)->generateArray($result);
+        foreach ($array AS $key => $value) {
+            unset($array[$key]['@ID']);
+            $array[$value['@ID']] = $array[$key];
+            unset($array[$key]);
+        }
+        return $array;
     }
 }
