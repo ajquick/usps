16f3f2bae5f88440dc69065b91ad07c949b04830
AJ Quick <ajquick@gmail.com>
2017-02-14 21:21:56 -0700

    Update Sanitization and Validation
    Added special case for when pattern matches ISO8601 format.

--- lib/Validation.php
+++ lib/Validation.php
@@ -87,7 +87,11 @@ class Validation
         }
         
         if (isset($rules['pattern'])) {
-            if (!preg_match('/^' . $rules['pattern'] . '$/', $value)) {
+			if ($rules['pattern'] == 'ISO 8601') {
+				$rules['pattern'] = '([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?';	
+			}
+			
+			if (!preg_match('/^' . $rules['pattern'] . '$/', $value)) {
                 $this->setError(sprintf("Invalid value %s does not match pattern '%s' for key %s.", $value, $rules['pattern'], $key));
                 return false;    
             }
